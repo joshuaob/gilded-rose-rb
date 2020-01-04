89e969a17a4f512e8324a95ec34f4b2344becd37
Joshua Ob <joshuaaob@gmail.com>
2020-01-04 20:46:08 +0000

    Define concrete classes for item types

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -3,51 +3,10 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      item.update_sell_in
+      item.update_quality
     end
   end
 end
@@ -63,5 +22,79 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+class Basic < Item
+  def update_quality
+    if quality < 50
+      sell_in < 0 ? self.quality -= 2 : self.quality -= 1
+    end 
+    self.quality = 0 if quality < 0
+  end
+
+  def update_sell_in
+    self.sell_in = sell_in - 1
+  end
+end
+
+class Legendary < Item
+  def update_quality
+  end
+
+  def update_sell_in
+    self.sell_in = sell_in - 1
+  end
+end
+
+class BackStagePass < Item
+  def update_quality
+    if quality < 50
+      self.quality = quality + 1
+
+      if name == 'Backstage passes to a TAFKAL80ETC concert'
+        if sell_in < 11
+          if quality < 50
+            self.quality = quality + 1
+          end
+        end
+        if sell_in < 6
+          if quality < 50
+            self.quality = quality + 1
+          end
+        end
+      end
+    end
+
+    if sell_in < 0
+      if name != 'Aged Brie'
+        if name != 'Backstage passes to a TAFKAL80ETC concert'
+          if quality > 0
+            if name != 'Sulfuras, Hand of Ragnaros'
+              self.quality = quality - 1
+            end
+          end
+        else
+          self.quality = quality - quality
+        end
+      end
+    end
+  end
+
+  def update_sell_in
+    self.sell_in = sell_in - 1
+  end
+end
+
+class Conjured < Item
+  def update_quality
+    if quality < 50
+      sell_in < 0 ? self.quality -= 4 : self.quality -= 2
+    end
+    self.quality = 0 if quality < 0
+  end
+
+  def update_sell_in
+    self.sell_in = sell_in - 1
   end
 end
